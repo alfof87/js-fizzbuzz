52324028e6e04a7a9cade8c03aee921a0af99ab6
Alfonso <alfof87@gmail.com>
2020-06-25 17:30:50 +0200

    prova 2

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ for(i = 1; i <= 100; i++){
 
 
   if(i%3 == 0){
-    console.log("Frizz");
+    console.log("Fizz");
   }else if (i%5 == 0) {
     console.log("Buzz");
   }else if (i%15 == 0) {
-    console.log("FrizzBuzz");
+    console.log("FizzBuzz");
 }else{
 
  }
