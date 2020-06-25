61a18196cd7103bd22bcf60ddf02a6b9202a5eab
Alfonso <alfof87@gmail.com>
2020-06-25 15:44:04 +0200

    prova 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// scrivi un programma che stampi i numeri da 1
+// a 100.
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
+// Per i multipli di 5 stampi "Buzz" al posto del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz"
+// al posto del numero.
+
+var numeri = [];
+
+for(i = 0; i < 100; i++){
+  if(numeri[i]%3 == 0){
+    console.log("Frizz")
+  }else if (numeri[i]%5 == 0) {
+    console.log("Buzz")
+  }
+}
